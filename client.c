95995ed072eb0abcbf3193e691aaf05e5967ce10
wujiahao <jiahaowu082@gmail.com>
2019-10-28 21:15:49 +0800

    revise log define

--- client.c
+++ client.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -8,12 +9,11 @@
 
 #define PORT 12306
 
-int main(int argc, char* argv[]) {
+int main() {
     // create socket
     int sockfd;
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("create socket failed");
-        exit(1);
+        log(ERROR,"create socket failed");
     }
     // prepare for connection
     struct sockaddr_in addr;
@@ -22,23 +22,20 @@ int main(int argc, char* argv[]) {
     addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     addr.sin_port = htons(PORT);
     if (connect(sockfd, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
-        perror("Connection failed in client");
-        exit(1);
+        log(ERROR,"Connection failed in client");
     }
 
     log(DEBUG, "Server connected.");
     // send message to server
     char snd[] = "Msg from client!";
     if (write(sockfd, snd, sizeof(snd)) < 0) {
-        perror("write failed");
-        exit(1);
+        log(ERROR,"write failed");
     }
     log(DEBUG, "send message to server!");
     // receive message from server
     char buff[100];
     if (read(sockfd, buff, 17) < 0) {
-        perror("read failed");
-        exit(1);
+        log(ERROR,"read failed");
     }
     log(DEBUG, "%s", buff);
     // close socket
--- http_server.c
+++ http_server.c
@@ -9,13 +9,11 @@
 
 #define SERVER_PORT 12306
 
-int main(){
+int main() {
     // create socket
     int sockfd;
-    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("create socket failed");
-        exit(1);
-    }
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+        log(ERROR, "Creating socket failed.");
 
     // bind ip and port
     struct sockaddr_in serv_addr;
@@ -23,10 +21,8 @@ int main(){
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     serv_addr.sin_port = htons(SERVER_PORT);
-    if (bind(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr))) {
-        perror("bind failed");
-        exit(1);
-    }
+    if (bind(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)))
+        log(ERROR, "bind failed");
 
     // listen and wait for connection from client
     listen(sockfd, 3);
@@ -36,26 +32,20 @@ int main(){
     int cs;
     struct sockaddr_in clnt_addr;
     socklen_t addr_size = (socklen_t)sizeof(clnt_addr);
-    if ((cs = accept(sockfd, (struct sockaddr*)&clnt_addr, &addr_size)) < 0) {
-        perror("accept failed");
-        exit(1);
-    }
+    if ((cs = accept(sockfd, (struct sockaddr*)&clnt_addr, &addr_size)) < 0)
+        log(ERROR, "accept failed");
     log(DEBUG, "Accept a connection from client.");
     // read data from client
     char buff[100];
-    if (read(cs, buff, 17) < 0) {
-        perror("read failed");
-        exit(1);
-    }
+    if (read(cs, buff, 17) < 0)
+        log(ERROR, "read failed");
     log(DEBUG, "%s", buff);
 
     // send data to client
     char str[] = "Msg from server!";
-    if (write(cs, str, sizeof(str)) < 0) {
-        perror("Write failed");
-        exit(1);
-    }
-   
+    if (write(cs, str, sizeof(str)) < 0)
+        log(ERROR, "Write failed");
+
     // close sockets
     close(cs);
     close(sockfd);
